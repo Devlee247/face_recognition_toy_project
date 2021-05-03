ca11984eefc2da797a21c3ecb2c63c3c910f59bd
2014031930 <inseo8070@naver.com>
2021-05-03 23:17:53 +0900

    recognition step : face detection & draw rectangle

--- model.py
+++ model.py
@@ -13,10 +13,35 @@ capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_COUNT, 640)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+face_cascade_name = 'haarcascade_frontalface_default.xml'
+
+face_cascade = cv2.CascadeClassifier()
+if not face_cascade.load(cv2.samples.findFile(face_cascade_name)):
+    print('Error loading face cascade')
+    exit(0)
+
 # ASCII 27 means 'esc'
 while cv2.waitKey(27) < 0:
     ret, frame = capture.read()
-    cv2.imshow("VideoFrame", frame)
+    # error handling
+    if not ret:
+        print("Can't receive frame. Exiting ...")
+        break
+    # convert frame's color to gray
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # detect faces
+    faces = face_cascade.detectMultiScale(
+        frame_gray,
+        minSize=(30,30)
+        )
+    print(faces)
+    # draw rectangle
+    for (x, y, w, h) in faces:
+        dst = cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0))
+
+    cv2.imshow("VideoFrame", dst)
+
+# When everything done, release the capture
 capture.release()
 cv2.destroyAllWindows()
